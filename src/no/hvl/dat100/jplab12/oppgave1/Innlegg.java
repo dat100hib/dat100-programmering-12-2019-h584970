b53611405c84962098d35c87cf8ab386e904df6c
steffenamland <steffenamland@158-37-226-232.hvl.no>
2019-11-11 23:04:29 +0100

    Oblig 3

--- src/no/hvl/dat100/jplab12/oppgave1/Innlegg.java
+++ src/no/hvl/dat100/jplab12/oppgave1/Innlegg.java
@@ -6,6 +6,12 @@ public abstract class Innlegg {
 	
 	// TODO - deklarering av objektvariable
 	
+	private int id;
+	private int likes; 
+	private String bruker; 
+	private String dato; 
+
+	
 	public Innlegg() {
 		
 	}
@@ -19,52 +25,91 @@ public abstract class Innlegg {
 	public Innlegg(int id, String bruker, String dato, int likes) {
 
 		// TODO - START
-		
-		throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
+		this.id = id; 
+		this.likes = 0; 
+		this.bruker = bruker; 
+		this.dato = dato;
+	
+		//throw new UnsupportedOperationException(TODO.constructor("Innlegg"));
 	}
 	
 	public String getBruker() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return bruker; 
+		
+		
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public void setBruker(String bruker) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		this.bruker = bruker;
+		
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public String getDato() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		
+		return dato; 
+		//throw new UnsupportedOperationException(TODO.method());
 		
 	}
 
 	public void setDato(String dato) {
+		
+		this.dato = dato;
+		
 		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public int getId() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		return id;
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 
 	public int getLikes() {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		return likes; 
+		
+		//throw new UnsupportedOperationException(TODO.method());
 
 	}
 	
 	public void doLike () {
-		throw new UnsupportedOperationException(TODO.method());
-	}
+		
+		likes ++; 
+			
+		
+		}
+		//throw new UnsupportedOperationException(TODO.method())
 	
 	public boolean erLik(Innlegg innlegg) {
-		throw new UnsupportedOperationException(TODO.method());
+		
+		if (innlegg.id == this.id)
+		{
+			return true;
+			
+		} else {
+			
+			return false;
+		}
 
-	}
+		}
+	
+		
+		//throw new UnsupportedOperationException(TODO.method());
+
 	
 	@Override
 	public String toString() {
 		
-		throw new UnsupportedOperationException(TODO.method());
+		return (id + "\n" + bruker + "\n" + dato + "\n" + likes + "\n");
+		
+		//throw new UnsupportedOperationException(TODO.method());
 				
 	}
 	
--- src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
+++ src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -1,17 +1,30 @@
 package no.hvl.dat100.jplab12.oppgave4;
-
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
 import no.hvl.dat100.jplab12.oppgave3.*;
 import no.hvl.dat100.jplab12.common.TODO;
-
 public class SkrivBlogg {
-
-	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
-
-	public static boolean skriv(Blogg samling, String filnavn) {
-
-		throw new UnsupportedOperationException(TODO.method());
-	}
+    private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
+    
+    public static boolean skriv(Blogg samling, String filnavn) {
+        PrintWriter skriver = null;
+        try {
+            File fil = new File(MAPPE+filnavn);
+            skriver = new PrintWriter(filnavn);
+            skriver.println(samling.toString());
+            
+            
+        }
+        catch(FileNotFoundException e) {
+            return false;
+            
+        }
+        finally {
+            if(skriver != null)
+                skriver.close();
+            return true;            
+    }
+        
+    }
 }
